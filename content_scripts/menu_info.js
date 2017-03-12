ecd2f4b95c280dcf1bca70396fc0d98e37f8f0b6
Euan Kemp <euank@euank.com>
2017-03-12 14:27:16 -0700

    Add further validation to content script

--- content_scripts/menu_info.js
+++ content_scripts/menu_info.js
@@ -1,3 +1,5 @@
+
+// origUrl attempts to convert a twitter image url into its ":orig" form.
 function origUrl(url) {
   var ndx = url.lastIndexOf(":");
   if(ndx >= 0) {
@@ -9,12 +11,18 @@ function origUrl(url) {
 document.addEventListener('contextmenu', function(ev) {
   var el = ev.target;
   if(el.tagName == "IMG") {
-    console.log("doing it for " + el.src);
+    if(el.src === "") {
+      return;
+    }
+    // TODO: maybe we should validate it's really a twitter url
     browser.runtime.sendMessage({twitterOrigUrl: origUrl(el.src)});
     return;
   }
-  if(el.parentElement.classList.contains("Gallery-content")) {
+  if(el.parentElement && el.parentElement.classList.contains("Gallery-content")) {
     var media = el.parentElement.querySelector(".Gallery-media > .media-image");
+    if(media === null) {
+      return;
+    }
     browser.runtime.sendMessage({twitterOrigUrl: origUrl(media.src)});
     return;
   }
